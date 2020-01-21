e4d9d9ade8294d34b5b649f439394b5010c10556
Daxter Onyx <daxter5onyx@gmail.com>
2020-01-21 18:46:27 +0100

    better jump

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(Collider2D))]
+public class GroundCheck : MonoBehaviour
+{
+	public bool IsGrounded { get; private set; }
+	public Collider2D TriggerGroundCheck;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		IsGrounded = true;
+	}
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		IsGrounded = true;
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		IsGrounded = false;
+	}
+}
